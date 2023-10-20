239593d7f3e9cc15fcd5b8bd31a582d125c90b83
naqib <naqib.asri.294@gmail.com>
2023-10-20 11:18:51 +0800

    venv installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Literature Review Sentences Classification Model",
+    version="0.0.1",
+    author="naqibasri",
+    author_email="naqib.asri.294@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
